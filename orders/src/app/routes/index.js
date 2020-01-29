75ca8bbbf566ecb49893e5918df6f3a600f2e71f
Nanjundaswamy V M <nanjunda777@gmail.com>
2020-01-30 00:34:34 +0530

    routes related changes

--- orders/src/app/routes/index.js
+++ orders/src/app/routes/index.js
@@ -8,22 +8,16 @@
 const express = require('express')
 const router = express.Router()
 
-//including all routes file
-const placeOrder = require('./placeOrder')
-const updateOrder = require('./updateOrder')
-const viewOrder = require('./viewOrder')
-const totalAmount = require('./totalAmount')
-const cancelOrder = require('./cancelOrder')
-const getOrderList = require('./getOrderList')
-const getOrdersAmount = require('./getOrdersAmount')
+//including service file
+const orderService = require('../services/orderservice')
 
 //defining routes
-router.get('/view/:id', viewOrder.viewOrder)
-router.post('/orderList', getOrderList.getOrderList)
-router.post('/ordersAmount', getOrdersAmount.getOrdersAmount)
-router.get('/totalAmount/:id', totalAmount.totalAmount)
-router.put('/update/:id', updateOrder.updateOrder)
-router.post('/place', placeOrder.placeOrder)
-router.patch('/cancel/:id', cancelOrder.cancelOrder)
+router.get('/view/:id', orderService.viewOrder)
+router.post('/orderList', orderService.getOrderList)
+router.post('/ordersAmount', orderService.getOrdersAmount)
+router.get('/totalAmount/:id', orderService.totalAmount)
+router.put('/update/:id', orderService.updateOrder)
+router.post('/place', orderService.placeOrder)
+router.patch('/cancel/:id', orderService.cancel)
 
 module.exports = router;
